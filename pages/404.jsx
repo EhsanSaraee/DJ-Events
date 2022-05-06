a4cb0cf7552c5d292f3cb0bdb3f7b83ee96a88f2
ehsan <ehsansaraee6570@yahoo.com>
2022-05-06 10:45:34 +0430

    create custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+import styles from '../styles/NotFound.module.css';
+
+const NotFound = () => {
+   return (
+      <Layout title="Page Not Found">
+         <div className={styles.error}>
+            <h1>404</h1>
+            <h4>Sorry, there is nothing here</h4>
+            <Link href="/">Go Back Home</Link>
+         </div>
+      </Layout>
+   );
+};
+
+export default NotFound;
